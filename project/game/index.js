6a30470fe0135937af21a0a110c054d4b970e212
Roma <khairom2005@gmail.com>
2024-11-26 12:56:19 +0500

    nebolishie pravki

--- project/game/index.js
+++ project/game/index.js
@@ -4,10 +4,104 @@ const words = [
     'игры',
     'сообщение'
 ];
+let gallowsCount = 6;
+let trueCount = 0;
+let falseCount = 0;
+let textCount = 6; 
 
 function game() {
+    createGallows();
     createWords(words);
     createElem(russianAlphabet, words);
+    createCounter(); 
+    kBord(words);
+    counter(trueCount, falseCount);
+    innerCount(falseCount, textCount); 
+}
+
+function createGallows() {
+    let gallowsContainer = document.createElement('div');
+    gallowsContainer.id = 'gallowsContainer';
+    gallowsContainer.style.position = 'relative';
+
+    let gallows = document.createElement('img');
+    let manBody = document.createElement('img');
+    let manHead = document.createElement('img');
+    let manHandOne = document.createElement('img');
+    let manHandTwo = document.createElement('img');
+    let manLegOne = document.createElement('img');
+    let manLegTwo = document.createElement('img');
+
+    manHead.id = 'man1';
+    manBody.id = 'man2';
+    manHandOne.id = 'man3';
+    manHandTwo.id = 'man4';
+    manLegOne.id = 'man5';
+    manLegTwo.id = 'man6';
+
+    gallows.src = './img/gallows.png'; 
+    manBody.src = './img/body.png';
+    manHead.src =  './img/head.png';
+    manHandOne.src = './img/hand-one.png'; 
+    manHandTwo.src = './img/hand-two.png'; 
+    manLegOne.src = './img/leg-one.png'; 
+    manLegTwo.src = './img/leg-two.png';
+
+    gallows.style.width = '300px';
+    gallows.style.height = '300px';
+
+    manHead.style.position = 'absolute';
+    manHead.style.top = '78px';
+    manHead.style.left = '208px';
+    manHead.style.display = 'none';
+
+    manBody.style.position = 'absolute';
+    manBody.style.top = '178px';
+    manBody.style.left = '255px';
+    manBody.style.display = 'none';
+
+    manHandOne.style.position = 'absolute';
+    manHandOne.style.top = '175px';
+    manHandOne.style.left = '191px';
+    manHandOne.style.display = 'none';
+
+    manHandTwo.style.position = 'absolute';
+    manHandTwo.style.top = '175px';
+    manHandTwo.style.left = '257px';
+    manHandTwo.style.display = 'none';
+
+    manLegOne.style.position = 'absolute';
+    manLegOne.style.top = '304px';
+    manLegOne.style.left = '191px';
+    manLegOne.style.display = 'none';
+
+    manLegTwo.style.position = 'absolute';
+    manLegTwo.style.top = '304px';
+    manLegTwo.style.left = '256px';
+    manLegTwo.style.display = 'none';
+
+    document.body.appendChild(gallowsContainer);
+    gallowsContainer.appendChild(gallows);
+    gallowsContainer.appendChild(manBody);
+    gallowsContainer.appendChild(manHead);
+    gallowsContainer.appendChild(manHandOne);
+    gallowsContainer.appendChild(manHandTwo);
+    gallowsContainer.appendChild(manLegOne);
+    gallowsContainer.appendChild(manLegTwo);
+}
+
+function createCounter() {
+    const counter = document.createElement('p');
+    counter.id = 'countP';
+    document.body.appendChild(counter);
+    counter.style.marginLeft = '50%';
+    counter.style.marginRight = '50%';
+    counter.innerText = '0/6';
+}
+
+function innerCount(falseCount, textCount) {
+    let text = document.getElementById('countP');
+    text.innerText = `${falseCount}/${textCount}`;
 }
 
 function createWords(arr) {
@@ -17,37 +111,63 @@ function createWords(arr) {
     containerWord.style.display = 'flex';
     containerWord.style.flexDirection = 'row';
     containerWord.style.gap = '20px';
-    containerWord.style.margin = '140px';
+    containerWord.style.width = '150px';
+    containerWord.style.marginLeft = 'auto';
+    containerWord.style.marginRight = 'auto';
+    containerWord.style.marginBottom = '50px';
+    containerWord.style.marginTop = '100px';
 
     for (let j = 0; j < arr[0].length; j++) {
         const word = document.createElement('div');
         word.id = `word${j}`;
         containerWord.appendChild(word);
         word.style.borderBottom = 'solid 2px black';
-        word.style.width = '30px';
+        word.style.width = '100%';
     }
+}
 
+function kBord(arr) {
     document.addEventListener('keypress', function(event) {
-        const pressedKey = event.key;
-        for (let j = 0; j < arr[0].length; j++) {
-            if (pressedKey === arr[0][j]) {
-                const value = document.getElementById(`word${j}`);
-                value.innerText = pressedKey;
-                colorButton(pressedKey);
+        const pressedKey = event.key.toLowerCase();
+        if (russianAlphabet.includes(pressedKey)) {
+            let isPartOfWord = false;
+
+            for (let j = 0; j < arr[0].length; j++) {
+                if (pressedKey === arr[0][j]) {
+                    const value = document.getElementById(`word${j}`);
+                    value.innerText = pressedKey;
+                    isPartOfWord = true;
+                }
+            }
+
+            const button = Array.from(document.querySelectorAll('button')).find(button => button.innerText === pressedKey);
+
+            if (isPartOfWord) {
+                if (button && getComputedStyle(button).backgroundColor !== 'rgb(0, 128, 0)') {
+                    trueCount++;
+                    counter(trueCount, falseCount);
+                    colorButton(pressedKey, 'green');
+                }
+            } else {
+                if (button && getComputedStyle(button).backgroundColor !== 'rgb(85, 0, 24)') {
+                    falseCount++;
+                    let resultGallows = document.getElementById(`man${falseCount}`);
+                    resultGallows.style.display = 'block';
+                    innerCount(falseCount, textCount);
+                    counter(trueCount, falseCount);
+                    colorButton(pressedKey, '#550018');
+                }
             }
         }
     });
 }
 
-function colorButton(symbol) {
+function colorButton(symbol, color) {
     const buttons = document.querySelectorAll('button');
-    buttons.forEach(button=> {
+    buttons.forEach(button => {
         if (button.innerText === symbol) {
-            button.style.backgroundColor = 'green';
-        } 
-        if (button.innerText !== symbol) {
-            button.style.backgroundColor = '#550018';
-        } 
+            button.style.backgroundColor = color;
+        }
     });
 }
 
@@ -56,11 +176,6 @@ function createElem(arr, arr2) {
     container.id = 'container';
     document.body.appendChild(container);
 
-    const textContainer = document.createElement('div');
-    textContainer.id = 'textContainer';
-    document.body.appendChild(textContainer);
-
-    /////////--------------style-----------////////
     document.body.style.boxSizing = 'border-box';
     container.style.display = 'flex';
     container.style.flexWrap = 'wrap';
@@ -68,10 +183,10 @@ function createElem(arr, arr2) {
     container.style.margin = '0 auto';
     container.style.justifyContent = 'center';
     container.style.gap = '5px';
-    /////////--------------style-----------////////
 
     for (let i = 0; i < arr.length; i++) {
         const button = document.createElement('button');
+        button.id = `button${i}`;
         button.innerText = arr[i];
         button.style.backgroundColor = 'rgb(59, 0, 161)';
         button.style.color = 'white';
@@ -93,11 +208,44 @@ function createElem(arr, arr2) {
                 }
             }
             if (isPartOfWord) {
-                button.style.backgroundColor = 'green';
+                if (getComputedStyle(button).backgroundColor !== 'rgb(0, 128, 0)') {
+                    trueCount++;
+                    button.style.backgroundColor = 'green';
+                    button.disabled = true;
+                    counter(trueCount, falseCount);
+                }
             } else {
-                button.style.backgroundColor = '#550018';
+                if (getComputedStyle(button).backgroundColor !== 'rgb(85, 0, 24)') {
+                    falseCount++;
+                    let resultGallows = document.getElementById(`man${falseCount}`);
+                    resultGallows.style.display = 'block';
+                    counter(trueCount, falseCount);
+                    innerCount(falseCount, textCount);
+                    button.style.backgroundColor = '#550018';
+                }
             }
         });
+    }
+}
+
+function counter(trueCount, falseCount) {
+    if (trueCount === words[0].length) {
+        setTimeout(() => {
+            alert('Победа');
+            let result = confirm('Хотите продолжить?');
+            if (result) {
+                location.reload();
+            }
+        }, 100);
+    }
+    if (falseCount === 6) {
+        setTimeout(() => {
+            alert('Поражение');
+            let result2 = confirm('Хотите продолжить?');
+            if (result2) {
+                location.reload();
+            }
+        }, 100);
     }
 }
 
